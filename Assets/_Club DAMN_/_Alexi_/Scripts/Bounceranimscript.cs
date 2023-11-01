ed65a8bb3ff5c289b6b39c0dc089689318d6e387
PexiAlexi <alexi.strandberg@yh.nackademin.se>
2023-11-01 16:23:00 +0100

    Vakten är klar.

--- Assets/_Club DAMN_/_Alexi_/Scripts/Bounceranimscript.cs
+++ Assets/_Club DAMN_/_Alexi_/Scripts/Bounceranimscript.cs
@@ -11,22 +11,27 @@ public class Bounceranimscript : MonoBehaviour
     private BoxCollider triggerBoxForBouncer;
 
     [SerializeField]
+    private BoxCollider triggerBoxForId;
+
+    [SerializeField]
     private GameObject IDcard;
 
 
 
 
-    void BacktoIdle()
+    void TalkingtoIdle()
     {
         animbouncer.SetBool("Istalking", false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("IDcard"))
+        if (other.gameObject.CompareTag("Player"))
         {
             animbouncer.SetBool("Istalking", true);
             triggerBoxForBouncer.enabled = false;
+            IDcard.SetActive(true);
+            triggerBoxForId.enabled = true;
 
         }
     }
