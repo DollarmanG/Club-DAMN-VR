ebcc3245f7797f363125d78949e8544c4ce05919
PexiAlexi <alexi.strandberg@yh.nackademin.se>
2023-11-02 11:36:47 +0100

    Fixade med ölen och lita animationer för bartendern.

---
+++ Assets/_Club DAMN_/_Alexi_/Prefabs/Bottle/Scripts/Bottle.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottle : MonoBehaviour
+{
+    [SerializeField] GameObject brokenBottlePrefab;
+    private Rigidbody rb;
+    private bool hasCollided = false;
+
+    // Ange tröskelhastighet och tröskelkollisionshastighet.
+    public float explosionThresholdVelocity = 10.0f;
+    public float collisionThresholdVelocity = 5.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (!hasCollided && rb.velocity.magnitude >= explosionThresholdVelocity)
+        {
+            Explode();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude >= collisionThresholdVelocity)
+        {
+            hasCollided = true;
+            Explode();
+        }
+    }
+  
+
+    void Explode()
+    {
+        GameObject brokenBottle = Instantiate(brokenBottlePrefab, transform.position, Quaternion.identity);
+        brokenBottle.GetComponent<BrokenBottle>().RandomVelocities();
+        Destroy(gameObject);
+    }
+}
